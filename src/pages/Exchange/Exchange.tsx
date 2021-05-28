c8d5bc425c5afe13b30c139e04734ca7cd7b8a38
Muntean Gratian <muntean.gm@pg.com>
2021-05-28 18:05:39 +0300

    Project setup

---
+++ src/pages/Exchange/Exchange.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Exchange = (props: any) => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-6">1</div>
+        <div className="col-6">2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Exchange;
